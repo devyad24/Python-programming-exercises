53dda3c6b7ec538c468ce9035e717f6aa2cc1f61
devyad24 <devyadav4211@gmail.com>
2024-12-28 00:21:43 +0530

    q15

--- solutions.py
+++ solutions.py
@@ -1,3 +1,4 @@
+from curses.ascii import isalpha, isdigit, islower, isupper
 import math
 import timeit
 from re import split
@@ -210,4 +211,110 @@ def q_10():
 # q_10()
 
 
-        
+'''
+q11:
+    Write a program which accepts a sequence of comma separated 4 digit binary numbers as its input and then check whether they are divisible by 5 or not. The numbers that are divisible by 5 are to be printed in a comma separated sequence.
+    Example:
+    0100,0011,1010,1001
+    Then the output should be:
+    1010
+'''      
+
+def q_11():
+    nums = [inp for inp in input().split(',')]
+    res = []
+    for n in nums:
+        #you can turn binary to decimal by typecasting string to base 2
+        dec = int(n,2)
+        if dec % 5 == 0:
+            res.append(n)
+    print(','.join(res))
+# q_11()
+
+'''
+q12:
+    Write a program, which will find all such numbers between 1000 and 3000 (both included) such that each digit of the number is an even number.
+    The numbers obtained should be printed in a comma-separated sequence on a single line.
+'''
+
+def q_12():
+    res = []
+    for num in range(1000, 3001):
+        org_num = num
+        while num != 0:
+            last_digit = num % 10
+            if last_digit % 2 != 0:
+                break
+            num //= 10
+        if num == 0:
+            res.append(str(org_num))
+    print(','.join(res))
+
+# q_12()
+
+'''
+q13:
+    Write a program that accepts a sentence and calculate the number of letters and digits.
+    Suppose the following input is supplied to the program:
+    hello world! 123
+    Then, the output should be:
+    LETTERS 10
+    DIGITS 3
+'''
+
+def q_13():
+    s = input()
+    letters = digits =  0
+
+    for c in s:
+        if c.isdigit():
+            digits += 1
+        elif c.isalpha():
+            letters += 1
+    print('LETTERS', letters)
+    print('DIGITS', digits)
+
+# q_13()
+
+'''
+q14:
+    Write a program that accepts a sentence and calculate the number of upper case letters and lower case letters.
+    Suppose the following input is supplied to the program:
+    Hello world!
+    Then, the output should be:
+    UPPER CASE 1
+    LOWER CASE 9
+'''
+
+def q_14():
+    s = input()
+    upper = lower = 0
+    for c in s:
+        if c.isupper():
+            upper += 1
+        elif c.islower():
+            lower += 1
+    print("UPPER", upper)
+    print("LOWER", lower)
+
+# q_14()
+
+'''
+q15:
+    Write a program that computes the value of a+aa+aaa+aaaa with a given digit as the value of a.
+    Suppose the following input is supplied to the program:
+    9
+    Then, the output should be:
+    11106
+'''
+
+def q_15():
+    a = input()
+    # "%s" is a format specifier for string just like in C
+    n1 = int( "%s" % a )
+    n2 = int( "%s%s" % (a,a) )
+    n3 = int( "%s%s%s" % (a,a,a) )
+    n4 = int( "%s%s%s%s" % (a,a,a,a) )
+    print(n1+n2+n3+n4)
+
+# q_15()
