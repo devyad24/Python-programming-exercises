5d562b23a9a85f06f7d4178ceaa7ce0a42bbec22
devyad24 <devyadav4211@gmail.com>
2025-01-02 01:21:02 +0530

    question 17

--- solutions.py
+++ solutions.py
@@ -318,3 +318,68 @@ def q_15():
     print(n1+n2+n3+n4)
 
 # q_15()
+
+'''
+q16:
+    Use a list comprehension to square each odd number in a list. The list is input by a sequence of comma-separated numbers.
+    Suppose the following input is supplied to the program:
+    1,2,3,4,5,6,7,8,9
+    Then, the output should be:
+    1,3,5,7,9
+'''
+
+def q_16():
+    odd_nums = [n for n in input().split(',') if int(n) % 2 != 0]
+    print(','.join(odd_nums))
+
+# q_16()
+
+'''
+q17:
+    Write a program that computes the net amount of a bank account based a transaction log from console input. The transaction log format is shown as following:
+    D 100
+    W 200
+
+    D means deposit while W means withdrawal.
+    Suppose the following input is supplied to the program:
+    D 300
+    D 300
+    W 200
+    D 100
+    Then, the output should be:
+    500
+'''
+
+def q_17():
+    net = 0 
+    while True:
+        transaction = input()
+        if transaction:
+            transaction = transaction.split(' ')
+            trans_type = transaction[0] 
+            trans_amount = transaction[1] 
+            #assuming if trans_type is not D then it would be W
+            net = (net + int(trans_amount)) if trans_type == 'D' else (net - int(trans_amount))
+        else:
+            break
+    print(net)
+
+# q_17()
+
+'''
+q18:
+    A website requires the users to input username and password to register. Write a program to check the validity of password input by users.
+    Following are the criteria for checking the password:
+    1. At least 1 letter between [a-z]
+    2. At least 1 number between [0-9]
+    1. At least 1 letter between [A-Z]
+    3. At least 1 character from [$#@]
+    4. Minimum length of transaction password: 6
+    5. Maximum length of transaction password: 12
+    Your program should accept a sequence of comma separated passwords and will check them according to the above criteria. Passwords that match the criteria are to be printed, each separated by a comma.
+    Example
+    If the following passwords are given as input to the program:
+    ABd1234@1,a F1#,2w3E*,2We3345
+    Then, the output of the program should be:
+    ABd1234@1
+'''
