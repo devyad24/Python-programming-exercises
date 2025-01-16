752c8d0c80d1b01f16902f598812f3320ea87979
devyad24 <devyadav4211@gmail.com>
2025-01-17 00:55:26 +0530

    question 20

--- solutions.py
+++ solutions.py
@@ -1,4 +1,5 @@
 from curses.ascii import isalnum, isalpha, isdigit, islower, isupper
+import itertools
 import math
 from operator import itemgetter
 import timeit
@@ -436,6 +437,18 @@ def q_19():
 
 # q_19()
 
+'''
+q20:
+    Define a class with a generator which can iterate the numbers, which are divisible by 7, between a given range 0 and n.
+'''
 
+#https://stackoverflow.com/questions/1756096/understanding-generators-in-python
+def q_20():
+    a = 0
 
-
+    while True:
+        b = a
+        a = a + 1
+        if b % 7 == 0:
+            yield b
+print(list(itertools.islice(q_20(), 100)))
